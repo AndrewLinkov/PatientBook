1480970391f75ec7d9159664a8885caf23ba903f
Андрей <vsscomp@mail.ru>
2023-12-15 15:46:11 +0300

    Initial commit

---
+++ src/main/java/com/example/PatientBook/PatientBookApplication.java
@@ -0,0 +1,14 @@
+package com.example.PatientBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientBookApplication.class, args);
+		System.out.println("PatientBook");
+	}
+
+}
---
+++ src/test/java/com/example/PatientBook/PatientBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatientBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
