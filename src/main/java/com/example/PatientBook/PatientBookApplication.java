5bb9164de3bdc2572000d72259f63e9992540dc1
Андрей <vsscomp@mail.ru>
2023-12-16 09:25:34 +0300

    connection to web sait

--- src/main/java/com/example/PatientBook/PatientBookApplication.java
+++ src/main/java/com/example/PatientBook/PatientBookApplication.java
@@ -8,7 +8,6 @@ public class PatientBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientBookApplication.class, args);
-		System.out.println("PatientBook");
 	}
 
 }
